eddb97a14ffd93aad1f18c6ead3f99e5b0ed55a2
Bruno Casas <brunocasas04@gmail.com>
2020-12-30 14:19:59 -0400

    Implements heroku deploy in GitHub actions

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,9 +1,19 @@
 const gulp = require('gulp')
 const simpleTypescript = require('gulp-simple-typescript')
+const run = require("gulp-run-command").default
+const json = require('./package.json')
 
-gulp.task('build', (cb) => {
-  gulp.src('src/**/*.*')
-    .pipe(simpleTypescript())
-    .pipe(gulp.dest('dist'))
+function build (cb) {
+  gulp.src('src/**/*')
+  .pipe(simpleTypescript())
+  .pipe(gulp.dest('dist'))
   cb()
-})
+}
+
+async function makeImage (cb) {
+  await run(`docker build -t ${json.name} . `)()
+} 
+makeImage.displayName = 'make-image'
+
+gulp.task('build', build)
+gulp.task('docker-build', gulp.series(build, makeImage))
